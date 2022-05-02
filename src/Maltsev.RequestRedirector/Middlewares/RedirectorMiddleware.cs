fa23aafc17b0c7b18bddf1f6b72b8cf01bfc16be
Victor Maltsev <default8700k@gmail.com>
2022-05-02 19:45:46 +0300

    Initial configuration.

---
+++ src/Maltsev.RequestRedirector/Middlewares/RedirectorMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Maltsev.RequestRedirector.Middlewares;
+
+public class RedirectorMiddleware
+{
+    private readonly string _httpClient;
+
+    public RedirectorMiddleware(RequestDelegate next, string httpClient)
+    {
+        this._httpClient = httpClient;
+    }
+
+    public Task InvokeAsync(HttpContext httpContext)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Maltsev.RequestRedirector/RedirectorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Maltsev.RequestRedirector.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace Maltsev.RequestRedirector;
+
+public static class RedirectorExtensions
+{
+    public static IApplicationBuilder UseRequestRedirector(this IApplicationBuilder app, string httpClient)
+    {
+        app.UseMiddleware<RedirectorMiddleware>(httpClient);
+        return app;
+    }
+}
