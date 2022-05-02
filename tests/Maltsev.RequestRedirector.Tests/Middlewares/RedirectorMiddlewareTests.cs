478d00180bd20a9c093bae46e82df9735775a9d9
Victor Maltsev <default8700k@gmail.com>
2022-05-02 21:01:29 +0300

    Add tests.

---
+++ tests/Maltsev.RequestRedirector.Tests/Middlewares/RedirectorMiddlewareTests.cs
@@ -0,0 +1,52 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using RichardSzalay.MockHttp;
+using System.Net;
+using Xunit;
+
+namespace Maltsev.RequestRedirector.Tests.Middlewares;
+
+public class RedirectorMiddlewareTests
+{
+    private readonly MockHttpMessageHandler _httpClientRedirector = new();
+
+    [Theory]
+    [AutoData]
+    public async Task RequestOnRedirector_ShouldBeRedirected(RequestModel model)
+    {
+        // setup
+        var requestRedirector = _httpClientRedirector
+            .When(HttpMethod.Post, $"https://redirector:5001/{model.RequestUrl}")
+            .WithHeaders(model.RequestHeaders)
+            .WithContent(model.RequestContentBody)
+            .Respond(
+                statusCode: HttpStatusCode.OK,
+                headers: model.ResponseHeaders,
+                mediaType: "application/json",
+                content: "abc"
+            );
+
+        using var host = await new WebServiceTestHost(_httpClientRedirector).StartAsync();
+
+        // act
+        var response = await host.SendRequestAsync(
+            requestMessage: new HttpRequestMessage(HttpMethod.Post, $"/api/redirector/{model.RequestUrl}"),
+            headers: model.RequestHeaders
+        );
+
+        // assert
+        response.Should().Be200Ok();
+        response.Content.Headers.ContentType.Should().Be("application/json");
+
+        foreach (var header in model.ResponseHeaders)
+        {
+            response.Should().HaveHeader(header.Key).And.BeValues(new[] { header.Value });
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+        content.Should().Be("abc");
+
+        _httpClientRedirector.GetMatchCount(requestRedirector).Should().Be(1);
+        _httpClientRedirector.VerifyNoOutstandingExpectation();
+    }
+}
---
+++ tests/Maltsev.RequestRedirector.Tests/RequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Maltsev.RequestRedirector.Tests;
+
+public class RequestModel
+{
+    public string RequestUrl { get; set; } = null!;
+    public IEnumerable<KeyValuePair<string, string>> RequestHeaders { get; set; } = null!;
+    public string RequestContentBody { get; set; } = null!;
+    public IEnumerable<KeyValuePair<string, string>> ResponseHeaders { get; set; } = null!;
+}
